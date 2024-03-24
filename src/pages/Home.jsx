72295790a6cd07da7a9c00319f42890b81025aaa
Solaimanislam <islamsi2001@gmail.com>
2024-03-25 02:40:47 +0600

    added all file

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../Components/Navbar";
+
+
+const Home = () => {
+    return (
+        <div >
+            <div className=" ">
+                <Navbar></Navbar>
+            </div>
+            <h1>Home page</h1>
+        </div>
+    );
+};
+
+export default Home;
